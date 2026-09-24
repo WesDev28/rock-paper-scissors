ab80bad5fb793ea1f00558032b49ae80cccebf9d
WesDev28 <309171504+WesDev28@users.noreply.github.com>
2026-09-24 16:13:30 +0100

    create getComputerChoice function. returning rock, paper, or scissors randomly.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+//Set up project structure
+
+//write logic to get computer choice
+//MAKE new function to get the computer choice
+//CALL the math random function to get a number
+//COMPARE the number between 0.33 0.66 1 to determine rock paper or scissors
+//LOG the the result.
+
+
+
+
+//write logic to get human choice
+//declare players score variables
+//write the logic to play a single round
+//write the logic to play the entire game
+
+
+function getComputerChoice() {
+    let rpsComp = Math.floor(Math.random()*100)+1;
+    if (rpsComp < 33) {
+        console.log("rock");
+    }
+    else if (rpsComp >= 33 && rpsComp <= 66) {
+        console.log("paper");
+    }
+    else if (rpsComp > 66) {
+        console.log("scissors");
+
+    }
+}
+
+getComputerChoice();
